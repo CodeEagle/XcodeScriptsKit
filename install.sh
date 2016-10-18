fd84f1e6326ce44630a809f4030e370f41128106
CodeEagle <stasura@hotmail.com>
2016-10-18 14:53:04 +0800

    fixed

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ if [ -d "$XCODE_SCRIPTS_KIT_LOCATION" ]; then
   if [ -d ".git" ]; then
     git reset --hard HEAD && git pull
   else
+    rm -rf $XCODE_SCRIPTS_KIT_LOCATION
     git clone --depth=1 "https://github.com/CodeEagle/XcodeScriptsKit.git"
   fi
 else
