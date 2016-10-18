a946ca9440179668681668838cb3a396e1adcd35
CodeEagle <stasura@hotmail.com>
2016-10-18 14:41:03 +0800

    remove compile error when no image in assets

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 TOOLS_LOCATION="/Users/$USER/bin"
 mkdir -p $TOOLS_LOCATION && cd $TOOLS_LOCATION
-git clone --depth=1 "https://github.com/CodeEagle/XcodeScriptsKit.git"
 XCODE_SCRIPTS_KIT_LOCATION="$TOOLS_LOCATION/XcodeScriptsKit"
+rm -rf XCODE_SCRIPTS_KIT_LOCATION
+git clone --depth=1 "https://github.com/CodeEagle/XcodeScriptsKit.git"
 cd $XCODE_SCRIPTS_KIT_LOCATION && rm -rf .git
