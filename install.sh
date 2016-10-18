a23266518ba8f6435b1514153057b45736841341
CodeEagle <stasura@hotmail.com>
2016-10-18 14:21:22 +0800

    fixed git type

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 
 TOOLS_LOCATION="/Users/$USER/bin"
 mkdir -p $TOOLS_LOCATION && cd $TOOLS_LOCATION
-git clone --depth=1 "git@github.com:CodeEagle/XcodeScriptsKit.git"
+git clone --depth=1 "https://github.com/CodeEagle/XcodeScriptsKit.git"
 XCODE_SCRIPTS_KIT_LOCATION="$TOOLS_LOCATION/XcodeScriptsKit"
 cd $XCODE_SCRIPTS_KIT_LOCATION && rm -rf .git
