e2d4a3e73536cdfed695fe462d70cf407f41a2ce
CodeEagle <stasura@hotmail.com>
2016-10-18 14:47:13 +0800

    change the way to update

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@
 TOOLS_LOCATION="/Users/$USER/bin"
 mkdir -p $TOOLS_LOCATION && cd $TOOLS_LOCATION
 XCODE_SCRIPTS_KIT_LOCATION="$TOOLS_LOCATION/XcodeScriptsKit"
-rm -rf XCODE_SCRIPTS_KIT_LOCATION
-git clone --depth=1 "https://github.com/CodeEagle/XcodeScriptsKit.git"
-cd $XCODE_SCRIPTS_KIT_LOCATION && rm -rf .git
+if [ -d "$XCODE_SCRIPTS_KIT_LOCATION" ]; then
+  if [ -d ".git" ]; then
+    git reset --hard HEAD && git pull
+  fi
+else
+  git clone --depth=1 "https://github.com/CodeEagle/XcodeScriptsKit.git"
+fi
